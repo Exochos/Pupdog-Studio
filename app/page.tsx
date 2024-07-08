45dc54e82d40942336daf07016fc1b25bc3e5f69
Jeremy Ward <exochos@gmail.com>
2024-07-08 16:03:34 -0700

    Updates to pupdog.studio
    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
-import ClientComponent from "./client-component"
+// import ClientComponent from "./client-component"
+//return <ClientComponent />
+import Image from "next/image"
 import { metadata } from "./metadata"
 import "styles/global.css"
 export { metadata }
 
 export default function Page() {
-  return <ClientComponent />
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="flex h-screen flex-col items-center justify-center ">
+          <Image src="/images/PupLogo.png" width={200} height={200} alt="logo" />
+          <h2 className="text-2xl font-bold text-center mt-4 text-gray-800">Coming Soon!</h2>
+        </div>
+      </div>
+    </>
+  )
 }
