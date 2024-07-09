a9231b7389240cfc28a8e7bcb5ce4277beb64686
Jeremy Ward <exochos@gmail.com>
2024-07-09 14:16:19 -0700

    addinf google analytics
    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react"
 import "styles/tailwind.css"
-
+import { initializeGA, logPageView } from "./utils/googleAnalytics"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    initializeGA("G-ZV2QJBY6FK")
+    logPageView()
+
+    const handleRouteChange = () => {
+      logPageView()
+    }
+
+    window.addEventListener("popstate", handleRouteChange)
+
+    return () => {
+      window.removeEventListener("popstate", handleRouteChange)
+    }
+  }, [])
+
   return (
     <html lang="en">
       <body>{children}</body>
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
     <>
       <div className="container mx-auto">
         <div className="flex h-screen flex-col items-center justify-center ">
-          <Image src="/images/PupLogo.png" width={200} height={200} alt="logo" />
+          <Image src="/images/PupLogo.png" width={400} height={400} alt="logo" />
           <h2 className="mt-4 text-center text-2xl font-bold text-gray-800">Coming Soon!</h2>
         </div>
       </div>
