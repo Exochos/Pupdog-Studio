79b18d249a28e8a90be4082cf6c6acbb789ea5b9
Jeremy Ward <exochos@gmail.com>
2024-07-08 18:27:06 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
       <div className="container mx-auto">
         <div className="flex h-screen flex-col items-center justify-center ">
           <Image src="/images/PupLogo.png" width={200} height={200} alt="logo" />
-          <h2 className="text-2xl font-bold text-center mt-4 text-gray-800">Coming Soon!</h2>
+          <h2 className="mt-4 text-center text-2xl font-bold text-gray-800">Coming Soon!</h2>
         </div>
       </div>
     </>
