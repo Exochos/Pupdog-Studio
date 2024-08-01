e619d61985d0d15820185041fc04134ac6c11932
Jeremy Ward <exochos@gmail.com>
2024-08-01 10:35:17 -0700

    Update page.tsx

--- app/maths/week6/page.tsx
+++ app/maths/week6/page.tsx
@@ -15,7 +15,7 @@ function Page(): JSX.Element {
   const [error, setError] = useState<string>("")
   const [permutations, setPermutations] = useState<number | null>(null)
   const [combinations, setCombinations] = useState<number | null>(null)
-  const [activeTab, setActiveTab] = useState<"permutations" | "combinations">("permutations")
+  const [activeTab, setActiveTab] = useState<"permutations" | "combinations">("combinations")
 
   // Error messages
   const errorMessages = {
@@ -30,6 +30,19 @@ function Page(): JSX.Element {
   const factorial = (n: number): number => {
     if (n === 0) return 1
     return n * factorial(n - 1)
+  }
+
+  // Calculate combinations
+  const calculateCombinations = () => {
+    const n = parseInt(N as string)
+    const r = parseInt(R as string)
+
+    if (isNaN(n) || n < 0 || isNaN(r) || r < 0 || r > n) {
+      setCombinations(null)
+      return
+    }
+
+    setCombinations(factorial(n) / (factorial(r) * factorial(n - r)))
   }
 
   // Handle button click
@@ -92,64 +105,139 @@ function Page(): JSX.Element {
             </button>
           </div>
 
-          <div className="my-4">
+          <div className="container">
             {activeTab === "combinations" && (
-              <article className="text-sm text-gray-700">
-                <p>
-                  In Discrete Mathematics, a combination is a selection of items from a larger pool, where the order of
-                  selection does not matter. Combinations are used to calculate the number of ways to choose{" "}
-                  <strong>r</strong> items from a set of <strong>n</strong> items. The formula for combinations is given
-                  by:
-                </p>
-                <br />
-                <BlockMath math={formula} />
-                <p className="mt-4">
-                  where <strong>n</strong> is the total number of items and <strong>r</strong> is the number of items to
-                  choose.
-                </p>
-                <hr className="my-4" />
-
-                <BlockMath math={combinationFormula + " = " + combinations} />
-              </article>
+              <div className="join join-vertical w-full">
+                <div className="collapse collapse-plus rounded-none border-y-2 border-black p-1">
+                  <input type="checkbox" id="collapse1" className="peer" defaultChecked />
+                  <label htmlFor="collapse1" className="text-bold collapse-title cursor-pointer text-black">
+                    Combinations
+                  </label>
+                  <div className="collapse-content peer-checked:block">
+                    <article className="text-sm text-gray-700">
+                      <p>
+                        In Discrete Mathematics, a combination is a selection of a group of items from a larger group,
+                        where the order of selection does not matter. Combinations are used to calculate the number of
+                        ways to choose <strong>r</strong> items from a set of <strong>n</strong> different items.
+                        <br />
+                        The formula for combinations is given by:
+                      </p>
+                      <br />
+                      <BlockMath math={formula} />
+                      <p className="mt-4">
+                        <strong>n</strong> is the total number of items
+                        <br />
+                        <strong>r</strong> is the number of items to choose.
+                      </p>
+                    </article>
+                  </div>
+                </div>
+                <div className="collapse collapse-plus rounded-none border-y-2 border-black p-1">
+                  <input type="checkbox" id="collapse2" className="peer" />
+                  <label htmlFor="collapse2" className="collapse-title cursor-pointer text-black">
+                    Calculate Combinations
+                  </label>
+                  <div className="collapse-content peer-checked:block">
+                    <hr className="mb-2 w-full border-gray-300" />
+                    <div className="flex items-start justify-between">
+                      <div className="gap flex flex-col">
+                        <input
+                          type="number"
+                          className="input input-bordered mb-2"
+                          placeholder="Enter the value of n"
+                          value={N}
+                          onChange={(e) => {
+                            setN(e.target.value)
+                            calculateCombinations()
+                          }}
+                        />
+                        <input
+                          type="number"
+                          className="input input-bordered mb-2"
+                          placeholder="Enter the value of r"
+                          value={R}
+                          onChange={(e) => {
+                            setR(e.target.value)
+                            calculateCombinations()
+                          }}
+                        />
+                      </div>
+                      <div className="mx-4 flex items-center justify-center px-2">
+                        <BlockMath math={combinationFormula + " = " + (combinations || 0)} />
+                      </div>
+                    </div>
+                    {error && <p className="mt-4 text-red-500">{error}</p>}
+                  </div>
+                </div>
+              </div>
             )}
             {activeTab === "permutations" && (
-              <article className="text-sm text-gray-700">
-                The formula for permutations is given by:
-                <BlockMath math={permutationFormula} />
-              </article>
+              <div className="join join-vertical w-full">
+                <div className="collapse collapse-plus p-1">
+                  <input type="checkbox" id="collapse3" className="peer" />
+                  <label htmlFor="collapse3" className="collapse-title cursor-pointer text-black">
+                    Permutations
+                  </label>
+                  <div className="collapse-content peer-checked:block">
+                    <article className="text-sm text-gray-700">
+                      <p>
+                        In Discrete Mathematics, a permutation is an arrangement of items in a specific order.
+                        Permutations are used to calculate the number of ways to choose <strong>r</strong> items from a
+                        set of <strong>n</strong> different items where the order of selection matters.
+                        <br />
+                        The formula for permutations is given by:
+                      </p>
+                      <br />
+                      <BlockMath math={formula} />
+                      <p className="mt-4">
+                        <strong>n</strong> is the total number of items
+                        <br />
+                        <strong>r</strong> is the number of items to choose.
+                      </p>
+                    </article>
+                  </div>
+                </div>
+                <div className="collapse collapse-plus p-1">
+                  <input type="checkbox" id="collapse4" className="peer" />
+                  <label
+                    htmlFor="collapse4"
+                    className="collapse-title cursor-pointer text-black peer-checked:bg-gray-200"
+                  >
+                    Calculate Permutations
+                  </label>
+                  <div className="collapse-content peer-checked:block">
+                    <div className="flex items-start justify-between">
+                      <div className="flex flex-col gap-2">
+                        <label className="text-gray-700">Enter the value of n:</label>
+                        <input
+                          type="number"
+                          className="input input-bordered"
+                          placeholder="Enter the value of n"
+                          value={N}
+                          onChange={(e) => setN(e.target.value)}
+                        />
+                        <label className="text-gray-700">Enter the value of r:</label>
+                        <input
+                          type="number"
+                          className="input input-bordered"
+                          placeholder="Enter the value of r"
+                          value={R}
+                          onChange={(e) => setR(e.target.value)}
+                        />
+                      </div>
+                      <div className="ml-4">
+                        <BlockMath math={permutationFormula + " = " + (permutations || 0)} />
+                      </div>
+                    </div>
+                    <button className="btn btn-primary mt-4" onClick={handleClick}>
+                      Calculate
+                    </button>
+                    {error && <p className="mt-4 text-red-500">{error}</p>}
+                  </div>
+                </div>
+              </div>
             )}
           </div>
-
-          <div className="form-control">
-            <label htmlFor="n" className="label">
-              Total Items (n)
-            </label>
-            <input
-              type="number"
-              id="n"
-              className="input input-bordered"
-              placeholder="Enter the total number of items"
-              value={N}
-              onChange={(e) => setN(e.target.value)}
-            />
-
-            <label htmlFor="r" className="label">
-              Items to Choose (r): {R}
-            </label>
-            <input
-              type="number"
-              id="r"
-              className="input input-bordered"
-              placeholder="Enter the number of items to choose"
-              value={R}
-              onChange={(e) => setR(e.target.value)}
-            />
-            <hr className="my-4" />
-            <button className="btn btn-outline btn-primary m-2" onClick={handleClick}>
-              Calculate {activeTab.charAt(0).toUpperCase() + activeTab.slice(1)}
-            </button>
-          </div>
-          {error && <p className="text-red-500">{error}</p>}
         </div>
       </div>
     </>
