f5dee6f8db05d6d89e388934605ddf689e392330
Jeremy Ward <exochos@gmail.com>
2024-08-04 22:32:08 -0700

    1
    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Script from "next/script"
 import { useEffect } from "react"
+import CookieConsent from "react-cookie-consent"
 import "styles/tailwind.css"
 import { initializeGA, logPageView } from "./utils/googleAnalytics"
 
@@ -24,6 +25,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         {children}
+        <CookieConsent
+          location="bottom"
+          buttonText="I understand"
+          cookieName="myWebsiteCookieConsent"
+          style={{ background: "#2B373B" }}
+          buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+          expires={150}
+        >
+         In order to comply with the GDPR we are required to inform you that we use cookies on this website.
+        </CookieConsent>
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9359140916151157"
