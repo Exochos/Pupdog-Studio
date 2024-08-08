187ebe99a9cca6b7a6ac27f63e78ffcf058656f2
Jeremy Ward <exochos@gmail.com>
2024-08-08 12:53:59 -0700

    Update page.tsx

--- app/maths/week8/page.tsx
+++ app/maths/week8/page.tsx
@@ -1,7 +1,8 @@
 "use client"
+import Head from "next/head"
 import Image from "next/image"
-import React, { useEffect, useState } from "react"
-import "katex/dist/katex.min.css" // Ensure KaTeX styles are included
+import React, { useState } from "react"
+import "katex/dist/katex.min.css"
 // @ts-ignore
 import { BlockMath, InlineMath } from "react-katex"
 
@@ -9,31 +10,39 @@ const Week8Page = () => {
   const [activeTab, setActiveTab] = useState("arithmetics")
   const [firstTerm, setFirstTerm] = useState(0)
   const [commonDifference, setCommonDifference] = useState(0)
-  const [position, setPosition] = useState(0)
-  const [result, setResult] = useState(0)
+  const [position, setPosition] = useState(1)
+  const [result, setResult] = useState("")
+  const [steps, setSteps] = useState([])
 
+  const calculateTerm = () => {
+    // Calculate the nth term of the arithmetic sequence
+    const newSteps = [
+      `a_n = ${firstTerm} + ${position - 1} \\cdot ${commonDifference}`,
+      `a_n = ${firstTerm + (position - 1) * commonDifference}`,
+    ]
 
+    // Clear previous results and steps
+    setSteps([])
+    setResult("")
 
-
-
-  const handleReset = () => {
-    //TODO: Implement reset functionality
+    // Add each step with a delay,
+    newSteps.forEach((step, index) => {
+      setTimeout(() => {
+        setSteps((prevSteps: never[]) => [...prevSteps, step])
+      }, index * 500)
+    })
   }
-
-  useEffect(() => {
-    handleReset()
-  }, [activeTab])
 
   return (
     <>
-      <head>
+      <Head>
         <title>Arithmetic Sequences | Geometric Sequences</title>
         <meta name="description" content="This application calculates Arithmetic Sequences and Geometric Sequences." />
         <meta
           name="keywords"
           content="Arithmetic Sequences, Geometric Sequences, Sequences, Maths, Mathematics, Calculator"
         />
-      </head>
+      </Head>
       <div className="container mx-auto flex items-center justify-center bg-white">
         <div
           className={`h-full w-full border-2 border-black p-2 shadow-xl transition-transform duration-300 ${
@@ -85,23 +94,17 @@ const Week8Page = () => {
                       </a>
                     </p>
                     <hr className="my-4" />
-                    <p className="ml-10 mt-4 text-sm text-gray-700">
+                    <span className="ml-10 mt-4 text-sm text-gray-700">
                       The formula for the nth term of an arithmetic sequence is given by:
-                      <BlockMath>a_n = a_1 + (n - 1) * d</BlockMath>
-                      <br />
+                      <BlockMath math="a_n = a_1 + (n - 1) \cdot d" />
                       where:
-                      <br />
-                      <InlineMath>a_n</InlineMath> is the nth term of the sequence
-                      <br />
-                      <InlineMath>a_1</InlineMath> is the first term of the sequence
-                      <br />
-                      <InlineMath>d</InlineMath> is the common difference between the terms
-                      <br />
-                      <InlineMath>n</InlineMath> is the position of the term in the sequence
-                    </p>
+                      <InlineMath math="a_n" /> is the nth term of the sequence,
+                      <InlineMath math="a_1" /> is the first term of the sequence,
+                      <InlineMath math="d" /> is the common difference,
+                      <InlineMath math="n" /> is the position of the term in the sequence.
+                    </span>
                   </article>
                 </div>
-                <br />
                 <div className="card m-2 mt-10 rounded-none border-2 border-black p-2 shadow-xl">
                   <article>
                     <div
@@ -110,48 +113,53 @@ const Week8Page = () => {
                     >
                       Calculator
                     </div>
-                    <p className="ml-10 mt-4 text-sm text-gray-700">
+                    <span className="ml-10 mt-4 text-sm text-gray-700">
                       This calculator can be used to calculate the nth term of an arithmetic sequence given the first
                       term, common difference, and position of the term in the sequence.
-                    </p>
+                    </span>
                     <hr className="my-4" />
                     <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
                       <div className="flex flex-col">
                         <label htmlFor="firstTerm" className="text-sm text-gray-700">
-                          First Term (a<sub>1</sub>):
+                          First Term (a_1):
                         </label>
                         <input
-                          type="text"
+                          type="number"
                           id="firstTerm"
                           name="firstTerm"
                           className="input input-bordered input-info my-2 w-full max-w-xs bg-white"
                           placeholder="Enter the first term"
-                          onChange={(e) => setFirstTerm(parseInt(e.target.value))}
+                          value={firstTerm}
+                          onChange={(e) => setFirstTerm(parseInt(e.target.value) || 0)}
                         />
-
                         <label htmlFor="commonDifference" className="text-sm text-gray-700">
                           Common Difference (d):
                         </label>
                         <input
-                          type="text"
+                          type="number"
                           id="commonDifference"
                           name="commonDifference"
                           className="input input-bordered input-info my-2 w-full max-w-xs bg-white"
                           placeholder="Enter the common difference"
+                          value={commonDifference}
+                          onChange={(e) => setCommonDifference(parseInt(e.target.value) || 0)}
                         />
-
                         <label htmlFor="position" className="text-sm text-gray-700">
                           Position (n):
                         </label>
                         <input
-                          type="text"
+                          type="number"
                           id="position"
                           name="position"
                           className="input input-bordered input-info my-2 w-full max-w-xs bg-white"
                           placeholder="Enter the position of the term"
+                          value={position}
+                          onChange={(e) => setPosition(parseInt(e.target.value) || 0)}
                         />
-
-                        <button className="btn btn-primary mt-4 max-w-xs border-none bg-blue-100 shadow-md hover:bg-blue-300">
+                        <button
+                          className="btn btn-primary mt-4 max-w-xs border-none bg-blue-100 shadow-md hover:bg-blue-300"
+                          onClick={calculateTerm}
+                        >
                           Calculate
                         </button>
                       </div>
@@ -161,10 +169,35 @@ const Week8Page = () => {
                             Result
                           </div>
                           <div className="ml-10 mt-4 text-sm text-gray-700">
-                            <p>
-                              <InlineMath>a_n = {firstTerm} + ({position} - 1) * {commonDifference}</InlineMath>
-                              <br />
-                            </p>
+                            <span>
+                              <BlockMath math="a_n = a_1 + (n - 1) \cdot d" />
+                              <BlockMath
+                                math={`a_${position} = ${firstTerm} + (${position} - 1) \\cdot ${commonDifference}`}
+                              />
+                              <div className="mt-4">
+                                {steps.map((step, index) => (
+                                  <div key={index} className="animate__animated animate__fadeIn">
+                                    <BlockMath math={step} />
+                                  </div>
+                                ))}
+                                {result && (
+                                  <div
+                                    className="result animate__animated animate__fadeIn"
+                                    style={{ animationDelay: `${steps.length * 500}ms` }}
+                                  >
+                                    <BlockMath math={result} />
+                                  </div>
+                                )}
+                              </div>
+                            </span>
+                            {result && (
+                              <div className="mt-4">
+                                <span className="text-sm text-gray-700">
+                                  The <InlineMath math={`${position}^{th}`} /> term of the sequence is{" "}
+                                  <InlineMath math={result} />.
+                                </span>
+                              </div>
+                            )}
                           </div>
                         </div>
                       </div>
@@ -174,10 +207,7 @@ const Week8Page = () => {
               </>
             )}
             {activeTab === "geometric" && (
-              <>
-                <h1 className="my-4 p-2 text-xl font-bold text-black">Geometric Sequences:</h1>
-                <div className="space-y-4"></div>
-              </>
+              <h1 className="my-4 p-2 text-xl font-bold text-black">Geometric Sequences:</h1>
             )}
           </div>
         </div>
