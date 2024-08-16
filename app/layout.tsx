7d3e97fc054ccaa4a65e251201658e83c2b4e932
Jeremy Ward <exochos@gmail.com>
2024-08-15 20:51:11 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,24 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="en">
-      <body>
-        {children}
-        <CookieConsent
-          location="bottom"
-          buttonText="I understand"
-          cookieName="myWebsiteCookieConsent"
-          style={{ background: "#2B373B" }}
-          buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-          expires={150}
-        >
-          In order to comply with the GDPR we are required to inform you that we use cookies on this website.
-        </CookieConsent>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9359140916151157"
-          crossOrigin="anonymous"
-        />
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
