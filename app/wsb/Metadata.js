73610567964c9a056037cc28c792403c1e3d13df
Jeremy Ward <exochos@gmail.com>
2024-08-15 20:39:38 -0700

    1
    1

---
+++ app/wsb/Metadata.js
@@ -0,0 +1,13 @@
+
+const Metadata = ({ title, description }) => {
+  return (
+    <head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="robots" content="index, follow" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+  )
+}
+
+export default Metadata
---
+++ app/wsb/WordCloud.js
@@ -0,0 +1,100 @@
+// components/WordCloud.js
+"use client";
+import Chart from "chart.js/auto"
+import { WordCloudController, WordElement } from "chartjs-chart-wordcloud"
+import { useEffect, useRef } from "react"
+
+// Register the WordCloud controller and element with Chart.js
+Chart.register(WordCloudController, WordElement)
+
+// Common Company Names
+const companyNames = new Set([
+  "aapl",
+  "apple",
+  "tesla",
+  "tsla",
+  "amc",
+  "gme",
+  "gamestop",
+  "nokia",
+  "nok",
+  "bb",
+  "blackberry",
+  "amzn",
+  "amazon",
+  "goog",
+  "google",
+  "msft",
+  "microsoft",
+  "fb",
+  "facebook",
+  "snap",
+  "snapchat",
+  "twtr",
+  "twitter",
+  "roku",
+  "shop",
+  "shopify",
+  "intel",
+  "intc",
+  "amd",
+])
+
+const WordCloud = ({ words }) => {
+  const canvasRef = useRef(null)
+  const chartInstanceRef = useRef(null)
+
+  useEffect(() => {
+    if (canvasRef.current && !chartInstanceRef.current) {
+      const canvas = canvasRef.current
+      const ctx = canvas.getContext("2d")
+
+      if (ctx) {
+        chartInstanceRef.current = new Chart(ctx, {
+          type: "wordCloud",
+          data: {
+            labels: words.map((d) => d.word),
+            datasets: [
+              {
+                data: words.map((d) => 4 + d.value * 10),
+              },
+            ],
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            plugins: {
+              legend: {
+                display: false,
+              },
+            },
+            elements: {
+              word: {
+                color: function (context) {
+                  const index = context.dataIndex
+                  const word = words[index].word.toLowerCase()
+                  return companyNames.has(word) ? "#FF0000" : "#000000"
+                },
+              },
+            },
+          },
+        })
+      }
+    }
+
+    return () => {
+      if (chartInstanceRef.current) {
+        chartInstanceRef.current.destroy()
+        chartInstanceRef.current = null
+      }
+    }
+  }, [words])
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-100">
+      <canvas ref={canvasRef} className="animate__animated animate__fadeIn max-h-full max-w-full"></canvas>
+    </div>
+  )
+}
+
+export default WordCloud
