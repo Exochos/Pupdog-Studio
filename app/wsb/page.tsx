39665168ad27f142f69c73a4bb0e277a67208dfe
Jeremy Ward <exochos@gmail.com>
2024-08-18 16:28:56 -0700

    Update page.tsx

--- app/wsb/page.tsx
+++ app/wsb/page.tsx
@@ -1,12 +1,20 @@
 import React from "react"
+import { logEvent } from "app/utils/googleAnalytics"
 import WordCloud from "./WordCloud"
 
 export const metadata = {
-  title: "WSB Word Cloud",
-  description: "A word cloud of the most common words used in WSB posts.",
+  title: "A WallStreet Bets (almost) live Word Cloud",
+  description: "A Word cloud which tracks the most recent (24 hours) posts on the WallStreetBets subreddit",
+  url: "https://dev.pupdog.studio/wsb",
+  keywords: ["Word Cloud", "WallStreetBets", "Reddit", "API", "Next.js", "Stonks", "GME", "AMC", "WSB"],
 }
 
 const Page = () => {
+
+  // Track page view
+  React.useEffect(() => {
+    logEvent("User", "Viewed Page", "WallStreetBets Word Cloud")
+  }, [])
   return (
     <>
       <WordCloud />
