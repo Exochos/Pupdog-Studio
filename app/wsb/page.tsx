ed704885b784c854e26c1f73ec1981ff4c098960
Jeremy Ward <exochos@gmail.com>
2024-08-18 22:00:33 -0700

    Update page.tsx

--- app/wsb/page.tsx
+++ app/wsb/page.tsx
@@ -3,7 +3,7 @@ import WordCloud from "./WordCloud"
 
 // Metadata for the page
 export const metadata = {
-  title: "A WallStreet Bets (almost) live Word Cloud",
+  title: "A WSB live Word Cloud",
   description: "A Word cloud which tracks the most recent (24 hours) posts on the WallStreetBets subreddit",
   url: "https://dev.pupdog.studio/wsb",
   keywords: ["Word Cloud", "WallStreetBets", "Reddit", "API", "Next.js", "Stonks", "GME", "AMC", "WSB"],
